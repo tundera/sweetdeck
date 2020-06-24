21559d6767794c00ca4ca63e09b4778419a3d22e
tundera <61833561+tundera@users.noreply.github.com>
2020-06-24 17:09:11 -0600

    Initial commit

---
+++ theme.js
@@ -0,0 +1,11 @@
+import customTheme from "@theme-ui/prism/presets/shades-of-purple.json";
+
+export const theme = {
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+
+  ...customTheme
+};
